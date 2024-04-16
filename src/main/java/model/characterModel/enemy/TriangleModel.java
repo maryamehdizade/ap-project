7c93474cb28215c2b44c55e2ea9064378808587e
unknown <maryam.mehdizadeh1384@gmail.com>
2024-04-16 18:56:15 +0330

    creating constructor for triangle

--- src/main/java/model/characterModel/enemy/TriangleModel.java
+++ src/main/java/model/characterModel/enemy/TriangleModel.java
@@ -1,10 +1,36 @@
 package model.characterModel.enemy;
 
+import model.characterModel.PlayerModel;
 import model.movement.Movable;
 
 public class TriangleModel implements Movable {
+
+    private int x1, y1, x2, y2, x3, y3;
+    private int speed;
+    private PlayerModel playerModel;
+
+    public TriangleModel(int x1, int y1, int x2, int y2, int x3, int y3, int speed, PlayerModel playerModel) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        this.x3 = x3;
+        this.y3 = y3;
+        this.speed = speed;
+        this.playerModel = playerModel;
+    }
     @Override
     public int move() {
+        double angle = Math.atan2(playerModel.getLocation().getY() - y1, playerModel.getLocation().getX() - x1);
+        int dx = (int) (Math.cos(angle) * speed);
+        int dy = (int) (Math.sin(angle) * speed);
+
+        x1 += dx;
+        y1 += dy;
+        x2 += dx;
+        y2 += dy;
+        x3 += dx;
+        y3 += dy;
         return 0;
     }
 
--- src/main/java/view/charactersView/enemy/TriangleView.java
+++ src/main/java/view/charactersView/enemy/TriangleView.java
@@ -1,15 +1,55 @@
 package view.charactersView.enemy;
 
+import model.characterModel.PlayerModel;
 import model.movement.Movable;
 
-public class TriangleView implements Movable {
-    @Override
-    public int move() {
-        return 0;
+import java.awt.*;
+
+public class TriangleView {
+    private int x1, y1, x2, y2, x3, y3;
+    private int hp;
+
+    public TriangleView(int x1, int y1, int x2, int y2, int x3, int y3) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        this.x3 = x3;
+        this.y3 = y3;
     }
 
-    @Override
-    public void move(double velocity) {
+    public void draw(Graphics g) {
+        int[] xPoints = {x1, x2, x3};
+        int[] yPoints = {y1, y2, y3};
+        g.setColor(Color.BLUE);
+        g.fillPolygon(xPoints, yPoints, 3);
+    }
 
+    public void setX1(int x1) {
+        this.x1 = x1;
+    }
+
+    public void setY1(int y1) {
+        this.y1 = y1;
+    }
+
+    public void setX2(int x2) {
+        this.x2 = x2;
+    }
+
+    public void setY2(int y2) {
+        this.y2 = y2;
+    }
+
+    public void setX3(int x3) {
+        this.x3 = x3;
+    }
+
+    public void setY3(int y3) {
+        this.y3 = y3;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
     }
 }
